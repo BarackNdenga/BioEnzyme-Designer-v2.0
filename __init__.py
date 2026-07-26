c533481af0210c6e66a6d15ca7af3ed8e5a36cae
BarackNdenga <ndengabarack@gmail.com>
2026-07-27 00:50:17 +0100

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+BioEnzyme Designer v2.0
+
+A comprehensive computational enzyme engineering toolkit featuring:
+  - Multi-chain (multimeric) enzyme analysis
+  - Public dataset integration (BRENDA, SABIO-RK)
+  - FoldX/Rosetta ΔΔG validation
+  - Tkinter graphical interface
+  - REST API for pipeline integration
+  - Cloud deployment (AWS, GCP)
+"""
+
+__version__ = "2.0.0"
+__author__ = "BioEnzyme Team"
